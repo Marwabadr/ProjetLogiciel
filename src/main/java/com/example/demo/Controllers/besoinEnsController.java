87d46619f56336265e437a876f2693d38b275675
houyamElAtlati <houyamel277@gmail.com>
2024-04-20 22:34:36 +0000

    h

--- src/main/java/com/example/demo/Controllers/besoinEnsController.java
+++ src/main/java/com/example/demo/Controllers/besoinEnsController.java
@@ -4,16 +4,23 @@ import com.example.demo.Models.Besoin;
 import com.example.demo.Services.BesoinEnsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 @Controller
 public class besoinEnsController {
     @Autowired
     private  BesoinEnsService besoinService;
 
-
-    @PostMapping("/ajouter")
-    public String ajouterBesoin(@ModelAttribute("ajouter") Besoin besoin) {
+    @PostMapping("/enseignant1")
+    public String ajouterBesoin(@ModelAttribute("enseignant1") Besoin besoin) {
         besoinService.ajouterBesoin(besoin);
-        return "redirect:/ajouter"; // Rediriger vers une page de succès ou une autre page appropriée
+        return "redirect:/enseignant1"; // Rediriger vers une page de succès ou une autre page appropriée
     }
+    @GetMapping("/enseignant1")
+    public String afficherFormulaire(Model model) {
+        model.addAttribute("enseignant1", new Besoin());
+        return "enseignant1"; // Le nom de votre fichier enseignant1.html sans l'extension
+    }
+
+
 }
