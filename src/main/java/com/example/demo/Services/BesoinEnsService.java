f1e3320416a168a60a035552b4c2ca4ac8a0b409
OmaLions <oumaima.chouhayd42@gmail.com>
2024-04-21 11:44:18 +0100

    BesoinEnsRepository

--- src/main/java/com/example/demo/Services/BesoinEnsService.java
+++ src/main/java/com/example/demo/Services/BesoinEnsService.java
@@ -5,18 +5,18 @@ import com.example.demo.repositories.BesoinEnsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class BesoinEnsService {
 
+    private BesoinEnsRepository besoinRepository;
 
-
-    private  BesoinEnsRepository besoinRepository;
+    @Autowired // Ajoutez cette annotation pour l'injection de dépendance
+    public BesoinEnsService(BesoinEnsRepository besoinRepository) {
+        this.besoinRepository = besoinRepository;
+    }
 
     public void ajouterBesoin(Besoin besoin) {
         // Logique métier pour ajouter un besoin
         besoinRepository.save(besoin);
     }
-
-
 }
